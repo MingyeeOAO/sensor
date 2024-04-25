71626065121ab3621c62874e0ff630dcbec0e345
Mingyee <87744503+MingyeeOAO@users.noreply.github.com>
2024-04-25 14:58:42 +0800

    Update Color.ts

--- lib/Color.ts
+++ lib/Color.ts
@@ -1,1 +1,13 @@
-// 在這裡添加你的程式
+class Color{
+    r : number
+    g : number
+    b : number
+
+    constructor(red : number, green: number, blue: number){
+        this.r = red; this.g = green; this.b = blue;
+    }
+
+    get hex(): number{
+        return neopixel.rgb(this.r, this.g, this.b);
+    }
+}
